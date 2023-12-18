dcff8ff7a0212ae6a6fd3fbd8a51fa84e49d8afe
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2023-12-18 16:41:09 +0700

    up eksplor dan detail-eksplor

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,46 @@
 // Owlcarousel
+// $(document).ready(function () {
+//     $(".owl-carousel").owlCarousel({
+//         loop: true,
+//         margin: 10,
+//         nav: true,
+//         autoplay: true,
+//         autoplayTimeout: 3000,
+//         autoplayHoverPause: true,
+//         center: true,
+//         navText: [
+//             "<i class='fa fa-angle-left'></i>",
+//             "<i class='fa fa-angle-right'></i>"
+//         ],
+//         responsive: {
+//             0: {
+//                 items: 1
+//             },
+//             600: {
+//                 items: 1
+//             },
+//             1000: {
+//                 items: 3
+//             }
+//         }
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#owl-demo").owlCarousel({
+//         navigation: true
+//     });
+// });
+
+
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
         autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: false,
         center: true,
         navText: [
             "<i class='fa fa-angle-left'></i>",
@@ -22,12 +56,7 @@ $(document).ready(function () {
             1000: {
                 items: 3
             }
-        }
-    });
-});
-
-$(document).ready(function () {
-    $("#owl-demo").owlCarousel({
-        navigation: true
+        },
+        autoplaySpeed: 2000, // Nilai default adalah 3000 (3 detik)
     });
 });
