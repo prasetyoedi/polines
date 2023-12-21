51f7edf8f6ef51eddf67fa3d032de8ab6c05c687
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2023-12-21 18:38:47 +0700

    up perbaikan bagian carousel

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
+    $(".owl-carousel.varian-pertama").owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
@@ -24,8 +24,33 @@ $(document).ready(function () {
         },
         autoplaySpeed: 4000,
     });
-});
 
+    $(".owl-carousel.varian-kedua").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: false,
+        center: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        },
+        autoplaySpeed: 4000,
+    });
+});
 
 function changeColor() {
     var icon = document.getElementById("myIcon");
